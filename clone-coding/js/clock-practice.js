93ebe27d27008410a7f862ba02e3be0b4f9ddb8e
joonho_Lee <goodfluence@naver.com>
2020-09-29 07:11:56 +0900

    JavaScript 시계 만들기 복습
    시계 웹 어플리케이션을 직접 만들어봤습니다.

---
+++ clone-coding/js/clock-practice.js
@@ -0,0 +1,49 @@
+const clockContainer = document.querySelector('.js-clock'),
+clockContent = document.querySelector('h1');
+
+function loadTime(){
+    const clock = new Date(),
+    year = clock.getFullYear(),
+    month = clock.getMonth(),
+    date = clock.getDate(),
+    hours = clock.getHours(),
+    minutes = clock.getMinutes(),
+    seconds = clock.getSeconds(),
+    day = clock.getDay(),
+    day_2 = daySelect(day);
+    
+    // 2020/09/29/06:30'30
+    
+    clockContent.textContent = `${year}/${month < 10 ? `0${month}` : month}/${date < 10 ? `0${date}` : date}/${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}'${seconds < 10 ? `0${seconds}` : seconds} ${day_2}day`;
+}
+
+function daySelect(day){
+    if (day==0){
+        day = 'Mon';
+        return day;
+    } else if (day==1){
+        day = 'Tues';
+        return day;
+    } else if (day==2){
+        day = 'Wednes';
+        return day;      
+    } else if (day==3){
+        day = 'Thurs';
+        return day;
+    } else if (day==4){
+        day = 'Fri';
+        return day;
+    } else if (day==5){
+        day = 'Satur';
+        return day;
+    } else if (day==6){
+        day = 'Sun';
+        return day;
+    }
+}
+
+function init(){
+    setInterval(loadTime, 1000);
+}
+
+init();
