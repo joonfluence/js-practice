0f164c24878dd85c2ad016fd9481239414b688f6
joonho_Lee <goodfluence@naver.com>
2020-09-29 15:32:26 +0900

    todo list 구현 - 추가하기

---
+++ clone-coding/js/todo-practice.js
@@ -0,0 +1,51 @@
+const toDoForm = document.querySelector('.js-toDoForm'),
+toDoInput = toDoForm.querySelector('input'),
+toDoList = document.querySelector('.js-toDoList');
+
+const TODOS_LS = "toDos";
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.textContent = "✔";
+    span.textContent = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.setAttribute('id', newId);
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos(toDoObj);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach()
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
