8a4972033bf727ea321f6d3611ba1ab0fa4dad37
joonho_Lee <goodfluence@naver.com>
2020-10-05 10:21:43 +0900

    내용 복습하기

--- clone-coding/js/todo-practice2.js
+++ clone-coding/js/todo-practice2.js
@@ -1,55 +1,69 @@
-const toDoForm = document.querySelector('.js-toDoForm'),
-toDoInput = toDoForm.querySelector('input'),
-toDoList = document.querySelector('.js-toDoList');
+const toDoForm = document.querySelector(".js-toDoForm"),
+toDoInput = toDoForm.querySelector("input"),
+toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = "toDos";
+const TODOS_LS = "ToDos";
+let toDos = [];
 
-const toDos = [];
-
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-function paintToDo(text) {
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
-  span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
-  toDoList.appendChild(li);
-  const toDoObj = {
-    text: text,
-    id: newId
-  };
-  toDos.push(toDoObj);
-  saveToDos();
+function deleteToDo(event){
+    const deletedToDo = event.target.parentNode;
+    console.log(deletedToDo.id);
+    toDoList.removeChild(deletedToDo);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(deletedToDo.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = "";
+function paintToDos(text){
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const delBtn = document.createElement('button');
+    const newId = toDos.length + 1;
+
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.setAttribute("id", newId);
+    toDoList.appendChild(li);
+    
+    delBtn.innerHTML = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+    span.innerHTML = text;
+
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
-function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
-      paintToDo(toDo.text);
+function handleSubmit(event){
+    event.preventDefault();
+    const textContent = toDoInput.value;
+    paintToDos(textContent);
+    toDoInput.value = '';
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDos(toDo.text);
         });
     }
 }
 
-
 function init(){
     loadToDos();
-    toDoForm.addEventListener('submit', handleSubmit);
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
--- clone-coding/js/todo.js
+++ clone-coding/js/todo.js
@@ -11,18 +11,19 @@ function saveToDos(){
 }
 
 // ToDoList는 사용자가 입력한 값을 바탕으로 새롭게 값을 추가하여야 하므로, createElement()로 HTML 태그를 만들어주고, 사용자가 입력한 값을 해당 태그 안에 입력해준다. 또한 DOM 구조 상, ul 태그 위에 li, li 태그 안에 span과 button을 넣어준다. (ul 태그는 기존에 존재하던 요소이다.)
-const li = document.createElement("li");
-const delBtn = document.createElement("button");
-const span = document.createElement("span");
-const newId = toDos.length + 1;
-delBtn.innerText = "✔";
-delBtn.addEventListener("click", deleteToDo);
-li.appendChild(delBtn);
-li.appendChild(span);
-li.id = newId;
-toDoList.appendChild(li);
+
 
 function paintToDo(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "✔";
+    delBtn.addEventListener("click", deleteToDo);
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDoList.appendChild(li);
     span.innerText = text;
     const toDoObj = {
         text: text,
@@ -38,14 +39,11 @@ function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
     toDoList.removeChild(li);
-    console.log(toDos);
-    console.log(toDos[0].id);
-    console.log(li.id);
     const cleanToDos = toDos.filter(function(toDo){
         return toDo.id !== parseInt(li.id);
     });
     toDos = cleanToDos;
-    saveToDos()
+    saveToDos();
 }
 
 // handleSubmit을 통해, 사용자가 입력한 값을 text로 건네주고 화면에 추가하는 일을 한다. 
@@ -57,7 +55,7 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
 
-// JSON.parse(loadedToDos)를 통해, String으로 변환됐던 값들을 다시 Object로 변환하였고 이제 해당하는 값들을 읽어올 수 있게 되었음. 
+// JSON.parse(loadedToDos)를 통해, String으로 변환됐던 값들을 다시 Object로 변환하였고 이제 해당하는 값들을 화면 상에 출력할 수 있게 되었음. 
 
 function loadToDos() {
     const loadedToDos = localStorage.getItem(TODOS_LS);
@@ -69,9 +67,9 @@ function loadToDos() {
     }
   }
 
-function init(){
-    loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit);
+function init(){ 
+    loadToDos(); // load할 ToDo가 있어야 실행됨 
+    toDoForm.addEventListener("submit", handleSubmit); // load할 ToDo가 없어도 실행됨 
 }
 
 init();
